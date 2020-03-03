ed286a52e52155f3109484ee7c9d017875d0c464
amalek1 <amalek1@ualberta.ca>
2020-03-02 22:27:28 -0700

    Updates

--- Helpers/FormHelper.cs
+++ Helpers/FormHelper.cs
@@ -14,10 +14,10 @@ namespace FillingForm.Helpers
             var attVal = element.GetAttribute(attName);
             return attVal;
         }
-        /*
-        public string GetSpanElementText(IWebDriver driver, string id)
+
+        public string GetSpanElementText(IWebDriver driver, string class1)
         {
-            var spanId = driver.FindElement(By.Id(id));
+            var spanId = driver.FindElement(By.ClassName(class1));
             return spanId.GetAttribute("span");
         }
         /*public string GetTextFieldValue2(IWebDriver driver, string xPath)
@@ -31,12 +31,12 @@ namespace FillingForm.Helpers
             var textField = driver.FindElement(By.Id(id));
             return textField.GetAttribute("value");
         }
-        
+
         public void ClickRadioOption(IWebDriver driver, string id, int optNum)
         {
             var radioButtonList = driver.FindElements(By.Id(id));
             var element = radioButtonList[optNum];
-            element.Click();            
+            element.Click();
         }
 
         // Visible if Test
@@ -45,5 +45,26 @@ namespace FillingForm.Helpers
             var visible = driver.FindElement(By.Id(id));
             return visible.GetAttribute("style");
         }
+        public void InsertValueJour(IWebDriver driver, string id, int optNum)
+        {
+            var ValueOpt = driver.FindElements(By.Id(id));
+            var element = ValueOpt[optNum];
+            element.Clear();
+            element.SendKeys("2");
+        }
+        public void InsertValueCata(IWebDriver driver, string id, int optNum)
+        {
+            var ValueOpt1 = driver.FindElements(By.Id(id));
+            var element = ValueOpt1[optNum];
+            element.Clear();
+            element.SendKeys("1");
+        }
+        
+        public void ClickOnButton(IWebDriver driver, string text)
+        {
+            var clickButton = driver.FindElement(By.LinkText(text));
+            clickButton.Click();
+        }
+
     }
 }
